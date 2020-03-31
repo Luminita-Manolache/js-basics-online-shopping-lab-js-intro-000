73a8e25a07fb7aae4e82e1eb84e0b0dff23a41d6
Luminita Manolache <luminitalumi.manolache@gmail.com>
2020-03-31 19:14:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function addToCart(item) {
  var newItem = {[item]: Math.floor( Math.random(1, 101) * 100 ) };
  cart.push(item);
 
-  return `${item} has been added to your cart.`;
+  return `${newItem} has been added to your cart.`;
 }
 
 function viewCart() {
